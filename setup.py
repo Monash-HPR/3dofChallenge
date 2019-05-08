f1716e9049cf51acbc2fe6eb4c5ae43be5e873a1
Caleb Fletcher <caleb@fletcher.cf>
2019-05-09 00:35:14 +1000

    Update build steps to allow NumPy C interface

--- setup.py
+++ setup.py
@@ -1,4 +1,9 @@
 from distutils.core import setup
 from Cython.Build import cythonize
+import numpy
 
-setup(name='3DOF', ext_modules=cythonize("Modules/*.pyx"))
+setup(
+    name='3DOF',
+    ext_modules=cythonize("Modules/*.pyx"),
+    include_dirs=[numpy.get_include()]
+)
