6ca4e4fd5f0c235dedd95ed3fe8e5c08853f1fa8
Caleb Fletcher <caleb@fletcher.cf>
2019-05-09 00:48:43 +1000

    Create Drag class

--- Modules/Aerodynamics.pyx
+++ Modules/Aerodynamics.pyx
@@ -1,5 +1,10 @@
 import logging
+cimport numpy as np
 
 logger = logging.getLogger(__name__)
+
+class Drag:
+    def __init__(self):
+        logger.debug('Creating Drag Instance')
 
 logger.debug('Aerodynamics Module Loaded')
