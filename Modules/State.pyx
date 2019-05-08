c4b3404b7e93e4f8b473d0ad9eae3df99b031f73
Caleb Fletcher <caleb@fletcher.cf>
2019-05-09 01:13:41 +1000

    Add State initialisation

--- Modules/State.pyx
+++ Modules/State.pyx
@@ -2,4 +2,11 @@ import logging
 
 logger = logging.getLogger(__name__)
 
+class State:
+    def __init__(self, **kwargs):
+        logger.debug('Creating State Instance')
+        self.time = kwargs.get('time', 0)
+        self.altitude = kwargs.get('altitude', 0)
+        self.velocity = kwargs.get('velocity', 0)
+
 logger.debug('State Module Loaded')
