98f7e17092a8590f206a0a5d491bfe9e9ceecd83
Caleb Fletcher <caleb@fletcher.cf>
2019-05-09 00:46:43 +1000

    Create base Motor class

--- Modules/Propulsion.pyx
+++ Modules/Propulsion.pyx
@@ -1,5 +1,10 @@
 import logging
+cimport numpy as np
 
 logger = logging.getLogger(__name__)
+
+class Motor:
+    def __init__(self):
+        logger.debug('Creating Motor Instance')
 
 logger.debug('Propulsion Module Loaded')
