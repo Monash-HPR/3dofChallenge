00fc53ffb0822c426ae7c4b21ee239e98012798c
Caleb Fletcher <caleb@fletcher.cf>
2019-05-09 01:12:51 +1000

    Add Integrator class

--- Modules/Integrator.pyx
+++ Modules/Integrator.pyx
@@ -2,4 +2,11 @@ import logging
 
 logger = logging.getLogger(__name__)
 
+class Integrator:
+    def __init__(self):
+        logger.debug('Creating Integrator Instance')
+
+    def RK4(self):
+        return
+
 logger.debug('Integrator Module Loaded')
