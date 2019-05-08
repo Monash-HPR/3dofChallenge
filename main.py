c2cc470840d783442ae540b848086a7c8c537716
Caleb Fletcher <caleb@fletcher.cf>
2019-05-09 00:04:31 +1000

    Add Logging System Use module-level logging with a global master

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-from Modules import *
+import logging
+
+# This should be before the Modules import so the logging level is shared
+logging.basicConfig(level=logging.DEBUG)
+logger = logging.getLogger(__name__)
+
+import Modules.Integrator
 
 if __name__ == "__main__":
-    print('Starting 3DOF Simulator')
+    logger.info('Starting 3DOF Simulator')
