bab9d821605cfac4340e0d1dc35ada97a6a6467e
Caleb Fletcher <caleb@fletcher.cf>
2019-05-09 00:21:49 +1000

    Add Simulator class

--- main.py
+++ main.py
@@ -6,5 +6,19 @@ logger = logging.getLogger(__name__)
 
 from Modules import Integrator, Aerodynamics, Propulsion, State
 
+class Simulator:
+    def __init__(self):
+        logger.debug('Creating Simulator Instance')
+        return
+
+    def initialise(self):
+        logger.info('Initialising Simulator')
+        return
+    
+    def run(self):
+        logger.info('Running Simulator')
+        return
+
 if __name__ == "__main__":
     logger.info('Starting 3DOF Simulator')
+    sim = Simulator()
