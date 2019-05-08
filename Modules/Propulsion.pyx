f9d66fd4898c74779fa28b62c25719da1581cce5
Caleb Fletcher <caleb@fletcher.cf>
2019-05-09 01:29:26 +1000

    Document the Motor class

--- Modules/Propulsion.pyx
+++ Modules/Propulsion.pyx
@@ -4,17 +4,39 @@ cimport numpy as np
 logger = logging.getLogger(__name__)
 
 class Motor:
-    def __init__(self):
+    def __init__(self, average_thrust, initial_mass, burnout_time):
+        """
+        Initialise a new Motor instance
+
+        """
         logger.debug('Creating Motor Instance')
 
-        self.initial_mass = 0
-        self.burnout_time = 0
-        self.average_thrust = 0
+        self.average_thrust = average_thrust
+        self.initial_mass = initial_mass
+        self.burnout_time = burnout_time
 
     def get_mass(self, time):
+        """
+        Get the motor mass at a given time.
+
+        Assumes a linear mass decrease.
+
+        :param time: The time in seconds
+        :type time: float
+        :returns: The mass in kilograms of the motor at the given time
+
+        """
         return
     
     def get_thrust(self, time):
+        """
+        Get the motor thrust at a given time.
+
+        Assumes a constant thrust for the duration of the motor burn.
+
+        :param time: The time in seconds
+        :returns: The motor thrust in Newtons at the given time
+        """
         return self.average_thrust if time < self.burnout_time else 0
 
 logger.debug('Propulsion Module Loaded')
