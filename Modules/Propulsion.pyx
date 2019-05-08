56bea41deaa84880ef9a2f17df63d91ff8eb6129
Caleb Fletcher <caleb@fletcher.cf>
2019-05-09 01:13:20 +1000

    Add basic Motor methods

--- Modules/Propulsion.pyx
+++ Modules/Propulsion.pyx
@@ -7,4 +7,14 @@ class Motor:
     def __init__(self):
         logger.debug('Creating Motor Instance')
 
+        self.initial_mass = 0
+        self.burnout_time = 0
+        self.average_thrust = 0
+
+    def get_mass(self, time):
+        return
+    
+    def get_thrust(self, time):
+        return self.average_thrust if time < self.burnout_time else 0
+
 logger.debug('Propulsion Module Loaded')
