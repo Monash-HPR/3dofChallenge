4e279ba93a43060bc690c0b1e0b03236479afff5
Caleb Fletcher <caleb@fletcher.cf>
2019-05-09 01:45:40 +1000

    Add drag coefficient calculation

--- Modules/Aerodynamics.pyx
+++ Modules/Aerodynamics.pyx
@@ -6,5 +6,8 @@ logger = logging.getLogger(__name__)
 class Drag:
     def __init__(self):
         logger.debug('Creating Drag Instance')
+    
+    def get_drag_coefficient(self, mach):
+        return 2400 * (np.exp(-1.2*M) * np.sin(M) + (M/6) * np.log10(M+1))
 
 logger.debug('Aerodynamics Module Loaded')
