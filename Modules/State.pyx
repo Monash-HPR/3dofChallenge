ff76fa5220b6ce98bf6fb79ddd50cefcfd4f2ec4
Caleb Fletcher <caleb@fletcher.cf>
2019-05-10 03:00:06 +1000

    Add utilities for state class

--- Modules/State.pyx
+++ Modules/State.pyx
@@ -1,3 +1,4 @@
+import json
 import logging
 
 logger = logging.getLogger(__name__)
@@ -6,7 +7,17 @@ class State:
     def __init__(self, **kwargs):
         logger.debug('Creating State Instance')
         self.time = kwargs.get('time', 0)
-        self.altitude = kwargs.get('altitude', 0)
-        self.velocity = kwargs.get('velocity', 0)
+        self.alt = kwargs.get('alt', 0)
+        self.vel = kwargs.get('vel', 0)
+    
+    def update(self, **kwargs):
+        self.__dict__.update(kwargs)
+    
+    def output(self, filename='output.json', stream=None):
+        if stream:
+            json.dump(self.__dict__, stream, separators=(',', ':'))
+        else:
+            with open(filename, 'w') as output_file:
+                json.dump(self.__dict__, output_file, separators=(',', ':'))
 
 logger.debug('State Module Loaded')
