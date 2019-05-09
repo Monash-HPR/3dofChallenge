ce7ee5820c4d9f1e55c6f8a2ef00ba6ef93dd46b
Caleb Fletcher <caleb@fletcher.cf>
2019-05-10 03:01:21 +1000

    Update simulator for diameter definition

--- main.py
+++ main.py
@@ -4,18 +4,21 @@ import logging
 logging.basicConfig(level=logging.DEBUG)
 logger = logging.getLogger(__name__)
 
-from Modules import Integrator, Aerodynamics, Propulsion, State
+from Modules import Integrator, Aerodynamics, Propulsion, State, Atmosphere
 logger.debug('All Modules Loaded')
 
 class Simulator:
-    def __init__(self):
+    def __init__(self, diameter):
         logger.debug('Creating Simulator Instance')
+
+        self.diameter = diameter
         self.motor = Propulsion.Motor(
             average_thrust=10.0,
-            initial_mass=2.0,
-            burnout_time=0.3
+            propellant_mass=2.0,
+            burnout_time=0.3,
+            casing_mass=1.0
         )
-        self.drag = Aerodynamics.Drag()
+        self.drag = Aerodynamics.Drag(diameter=self.diameter)
         self.state = State.State()
 
     def initialise(self):
@@ -26,4 +29,6 @@ class Simulator:
 
 if __name__ == "__main__":
     logger.info('Starting 3DOF Simulator')
-    sim = Simulator()
+    sim = Simulator(
+        diameter=0.1
+    )
