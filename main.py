ec0d24fdaeb9e066b53507e7e01b734845464582
Ciaran Anderson <Ciaran3105@gmail.com>
2019-07-09 00:50:08 +1000

    Expanded initial conditions

--- main.py
+++ main.py
@@ -6,12 +6,14 @@ import time
 initial_conditions = {
     "time": 0.0,            # Time to start simulation (s)
     "dt_initial": 0.1,      # Initial time step size
-    "mass_dry": 10,         # Mass of the rocket without propellant
-    "mass_propellant": 1,   # Mass of propellant
+    "mass_dry": 25,         # Mass of the rocket without propellant
+    "mass_propellant": 9,   # Mass of propellant
     "latitude": -37.8136,   # Range latitude    (degrees)
     "longitude": 144.9631,  # Range longitude   (degrees)
-    "altitude": 100
-}
+    "altitude": 100,        # Range altitude    (m)
+    "burn_time": 3.5,       # motor burn Time   (s)
+    "thrust": 5800          # motor burn Time   (N)
+
 
 
 # Initialise the state class with initial conditions
