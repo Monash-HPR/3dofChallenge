0c0e046393130e0c63ac71572a32bf770e0a5072
Ciaran Anderson <Ciaran3105@gmail.com>
2019-07-09 00:59:34 +1000

    added T_DG transform, not working as initial state vector is wrong

--- Modules/Transformations.py
+++ Modules/Transformations.py
@@ -9,29 +9,6 @@ def skewSymmetricExpansion(vector):
     x3 = vector[2]
     return np.array([ [0.0, -x3, x2], [x3, 0.0, -x1], [-x2, x1, 0]])
 
-def get_T_DG(State):
-    getGeodeticPosition(State)
-
-def initial_T_DG(sBI__G): #unsure if this is needed
-    geocentric_lat = Geosdesy.getGeocentricLatitude(State)
-    geodetic_lat = geocentric_lat
-    geodetic_lat_last = geodetic_lat + 1
-    sBI_norm = np.linalg.norm(State.sBI__I)
-    iter = 0
-
-    # Perform iterations to calculate geodetic latitude
-    while (np.abs(geodetic_lat_last - geodetic_lat) > 1e-6) and (iter < 15):
-        geodetic_lat_last = geodetic_lat
-        r = getR0(geodetic_lat)
-        h = sBI_norm - R0
-        delta = Geodesy.getApproximateDeflectionAngle(h)
-        geodetic_lat = delta + geocentric_lat
-        iter += 1
-
-    # Now calculate geodetic longitude as a correction is required due to the rotation of the Earth
-    geodetic_lon = arcsin(state.sBI_I_I[2]/np.sqrt(state.sBI_I_I[1]**2 + state.sBI_I_I[1]**2)) - lGO - omega_earth(State.time)
-    return
-
 def get_T_DI(sBI__I):
     # Create the transformation matrix from geodetic coordinates to inertial coordinates
     geodetic_postion = Geodesy.getGeodeticPosition(sBI__I)
@@ -41,3 +18,9 @@ def get_T_DI(sBI__I):
     return np.array([   [-np.sin(lat) * cos(lon), -np.sin(lat) * sin(lon), np.cos(lon)],
                         [-np.sin(lon), np.cos(lon), 0.0],
                         [-np.cos(lat) * np.cos(lon), -np.cos(lon) * np.sin(lon), -np.sin(lat)]])
+
+def get_T_DG(sBI__I):
+    delta = getDeflectionAngle(sBI__I) # This wont be the correct delta, Needs addressing
+    return np.array([   [np.cos(delta), 0.0, np.sin(delta)]
+                        [0, 1, 0]
+                        [-np.sin(delta), 0, np.cos(delta)]])
