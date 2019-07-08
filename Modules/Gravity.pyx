655ffff9dc0e94c02a8f9ed447ebd4eb8ef9f4ae
Caleb Fletcher <caleb@fletcher.cf>
2019-07-08 18:55:07 +1000

    Simplify gravity calculations

--- Modules/Gravity.pyx
+++ Modules/Gravity.pyx
@@ -2,15 +2,17 @@ import logging
 cimport numpy as np
 import numpy as np
 
-cdef double G = 6.67408e-11
-cdef double MASS_EARTH = 5.9722e24
+cdef double GM = 3.986005e14
 cdef double RADIUS_EARTH = 6371000.0
 
 logger = logging.getLogger(__name__)
 
 cdef class Gravity:
     def __init__(self):
+        logger.debug('Creating Gravity Instance')
         return
     
-    cpdef get_force(self, double alt, double mass):
-        return np.array([0.0, -G*MASS_EARTH*mass/np.power(RADIUS_EARTH+alt, 2), 0.0], np.float64)
+    cpdef get_acceleration(self, double alt):
+        return np.array([0.0, -GM/np.power(RADIUS_EARTH+alt, 2), 0.0], np.float64)
+
+logger.debug('Gravity Module Loaded')
