ec31d7cefe3f48d1ec1dd5fe00be47267ae79ab2
Caleb Fletcher <caleb@fletcher.cf>
2019-07-08 18:55:45 +1000

    Fix atmospheric calculations

--- Modules/Atmosphere.pyx
+++ Modules/Atmosphere.pyx
@@ -4,9 +4,9 @@ import numpy as np
 
 cdef double GAS_CONSTANT = 286.0
 cdef double SPEC_HEAT_RATIO = 1.4
-cdef double LAPSE_RATE = 0.00649 # NOTE: Only valid to 11000m (36k ft)
+cdef double LAPSE_RATE = 0.00649
 cdef double KELVIN_OFFSET = 273.15
-cdef double ATMOSPHERIC_TEMP_OFFSET = 15.04
+cdef double ATMOSPHERIC_TEMP_OFFSET = 288.15
 
 logger = logging.getLogger(__name__)
 
@@ -16,9 +16,23 @@ cpdef np.ndarray mach_to_ms(np.ndarray vel, double alt):
 cpdef np.ndarray ms_to_mach(np.ndarray vel, double alt):
     return np.divide(vel, get_speed_of_sound(alt))
 
-cpdef double get_density(double alt):
+cpdef double get_pressure(double alt):
+    """
+    Get the atmospheric pressure in Pascals.
+    Assumes tropospheric altitudes (<11km)
+
+    :param alt: Altitude in metres
+    :type alt: double
+    :returns: Density in Pascals
+
+    """
     cdef double temp = get_atmospheric_temp(alt)
-    return 101.29 * np.power((temp)/288.08, 5.256)
+    return 101325 * np.power(temp/288.15, 5.2559)
+
+cpdef double get_density(double alt):
+    cdef double pressure = get_pressure(alt)
+    cdef double temp = get_atmospheric_temp(alt)
+    return pressure/(GAS_CONSTANT * temp)
 
 cpdef double get_speed_of_sound(double alt):
     cdef double temp = get_atmospheric_temp(alt)
@@ -31,7 +45,7 @@ cpdef double celsius_to_kelvin(double temp):
     return temp + KELVIN_OFFSET
 
 cpdef double get_atmospheric_temp(double alt):
-    cdef double temp_celsius = ATMOSPHERIC_TEMP_OFFSET - LAPSE_RATE * alt
-    return celsius_to_kelvin(temp_celsius)
+    cdef double temp_kelvin = ATMOSPHERIC_TEMP_OFFSET - LAPSE_RATE * alt
+    return temp_kelvin
 
 logger.debug('Atmosphere Module Loaded')
