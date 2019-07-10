45126e2da8b61771e1d46279fcfb69b3d251fcfd
Ciaran Anderson <Ciaran3105@gmail.com>
2019-07-10 13:41:06 +1000

    Created get aerodynamic force function

--- Modules/Forces/Aerodynamics.py
+++ Modules/Forces/Aerodynamics.py
@@ -5,3 +5,11 @@ def getDragCoefficient(State):
     # Calculates that drag coefficent at a given Mach number from an approximate analytical function
     M = Atmosphere.getMach(State)
     return 2400 * (np.exp(-1.2 * M) * np.sin(M) + (M / 6) * np.log10(M + 1))
+
+def getAerodynamicForce(State):
+    # Returns the aerodynamic force vector in velocity coordinates
+    # Returning the force in velocity coords is only acceptable in a 3DOF simulation without horizontal winds
+    cD = getDragCoefficient(State)
+    s = State.reference_area
+    q = Atmosphere.getDynamicPressure(State)
+    return q * s * np.array([ [-cD], [0.0], [0.0]])
