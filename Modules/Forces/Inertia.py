b55f0cc7024e10b74d96df8cee3cc7aedc7d4b66
Ciaran Anderson <Ciaran3105@gmail.com>
2019-07-10 13:42:54 +1000

    Fixed get gravity vector

--- Modules/Forces/Inertia.py
+++ Modules/Forces/Inertia.py
@@ -1,10 +1,14 @@
 import numpy as np
 from Modules import Geodesy
+from Modules import Transformations
 
 def getGravityForce(State):
     # Calculate gravitation acccleration
     g = get_g__G(State.sBI__I)
 
     # Calculate centrifugal force due to Earth's Rotation
+    # wEI__I = Geodesy.get_WBE__I()
+    # omegaEI__I
+    # FIX THIS LATER
 
-    return
+    return g
