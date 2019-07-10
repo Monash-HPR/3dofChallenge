7e23cd88641bb428497d8a7929dc88b2b329e1ed
Ciaran Anderson <Ciaran3105@gmail.com>
2019-07-10 13:42:27 +1000

    Added get specific force function to integrator

---
+++ Modules/Integrator.py
@@ -0,0 +1,13 @@
+import numpy as np
+from Forces import Propulsion
+from Forces import Aerodynamics
+from Forces import Inertia
+
+def getSpecificForce(State):
+    # Returns the specific force (acceleration) due to propulsion and aerodynamics in velocity coordinates
+    # NOTE: This function requires uprading once wind is implemented as it assumes that velocity and body coordinates
+    # the same. Once wind is implemented, the aerodynamic force will be calculate
+    T = Propulsion.getThrust(State)
+    F = Aerodynamics.getAerodynamicForce(State)
+    m = State.mass
+    return  1/m * np.add(T, F)
