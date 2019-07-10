27c6efa123f4117a9137966a65b45836597985cc
Ciaran Anderson <Ciaran3105@gmail.com>
2019-07-10 13:42:03 +1000

    added reference area to initial conditions

--- main.py
+++ main.py
@@ -4,15 +4,16 @@ import time
 
 # Dictionary below that contains the initial conditions required for simulation
 initial_conditions = {
-    "time": 0.0,            # Time to start simulation (s)
-    "dt_initial": 0.1,      # Initial time step size
-    "mass_dry": 25,         # Mass of the rocket without propellant
-    "mass_propellant": 9,   # Mass of propellant
-    "latitude": -37.8136,   # Range latitude    (degrees)
-    "longitude": 144.9631,  # Range longitude   (degrees)
-    "altitude": 0,        # Range altitude    (m)
-    "burn_time": 3.5,       # motor burn Time   (s)
-    "thrust": 5800          # motor burn Time   (N)
+    "time": 0.0,                # Time to start simulation (s)
+    "dt_initial": 0.1,          # Initial time step size
+    "mass_dry": 25,             # Mass of the rocket without propellant
+    "mass_propellant": 9,       # Mass of propellant
+    "latitude": -37.8136,       # Range latitude    (degrees)
+    "longitude": 144.9631,      # Range longitude   (degrees)
+    "altitude": 0,              # Range altitude    (m)
+    "burn_time": 3.5,           # motor burn Time   (s)
+    "thrust": 5800,             # motor burn Time   (N)
+    "reference_area": 0.013893  # reference area (tube cross-section) (m^2)
     }
 
 
