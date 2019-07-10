cff2e1519a1c7804c65549746b81af71d4ab2585
Ciaran Anderson <Ciaran3105@gmail.com>
2019-07-10 16:01:18 +1000

    Fixed inertial force calculation

--- Modules/Forces/Inertia.py
+++ Modules/Forces/Inertia.py
@@ -2,13 +2,13 @@ import numpy as np
 from Modules import Geodesy
 from Modules import Transformations
 
-def getGravityForce(State):
+def getGravityAcceleration(State):
     # Calculate gravitation acccleration
     g = get_g__G(State.sBI__I)
 
     # Calculate centrifugal force due to Earth's Rotation
-    # wEI__I = Geodesy.get_WBE__I()
-    # omegaEI__I
-    # FIX THIS LATER
+    wEI__I = Geodesy.get_WBE__I()
+    omegaEI__I =Transformations.skewSymmetricExpansion(wEI__I)
+    f = np.matmul(np.matmul(omegaEI__I,omegaEI__I),State.sBI__I)
 
-    return g
+    return g-f
